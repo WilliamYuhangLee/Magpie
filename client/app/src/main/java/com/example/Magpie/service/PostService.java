ca4a9e1cef2de2b162a1aa9bfad0efed573b1b77
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2020-01-21 16:31:54 -0500

    Complete PostService API functions

--- client/app/src/main/java/com/example/Magpie/service/PostService.java
+++ client/app/src/main/java/com/example/Magpie/service/PostService.java
@@ -1,14 +1,34 @@
 package com.example.Magpie.service;
 
+import com.example.Magpie.model.Post;
 import com.example.Magpie.model.Response;
 
+import java.util.List;
+
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface PostService {
 
+    @POST("post/new")
+    Call<Response<Post>> create(@Query("username") String username, @Body Post post);
+
     @GET("post/{postId}")
-    Call<Response<POST>> get(@Path(value = "postId") long postId);
+    Call<Response<Post>> read(@Path(value = "postId") long postId);
+
+    @GET("post/")
+    Call<Response<List<Post>>> getAllFrom(@Query("username") String username);
+
+    @PUT("post/{postId}")
+    Call<Response<Post>> update(@Path(value = "postId") long postId, @Body Post post);
+
+    @DELETE("post/{postId}")
+    Call<Response<Void>> delete(@Path(value = "postId") long postId);
+
 }
